fec1ed2b1066fd1a6445ac33264cb1bd8e18e10f
nostopgmaming17 <noamshukrun2008@gmail.com>
2025-09-25 03:52:37 +0300

    dist -> out

--- minifybundle.bat
+++ minifybundle.bat
@@ -1,5 +1,5 @@
 @echo off
-if not exist "dist/" (
-    mkdir dist
+if not exist "out/" (
+    mkdir out
 )
-lua bundle.lua bundle.lua -o dist/bundle.lua -d BUNDLED=true
+lua bundle.lua bundle.lua -o out/bundle.lua -d BUNDLED=true
